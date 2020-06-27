69b549214664a0aab9450959e9afbe03d7c6f5e8
subhjoydey <subhojoy16@gmail.com>
2020-06-27 13:39:44 -0400

    62720201230

--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,7 @@ var typingName = "";
 
 
 
-var siteWidth = 1280;
+var siteWidth = 800;
 var scale = screen.width / siteWidth;
 
 document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=' + siteWidth + ', initial-scale=' + scale + '');
