3f1f1271cb83a47eb741a179f480fa569fa81f9a
subhjoydey <subhojoy16@gmail.com>
2020-07-03 12:59:21 -0400

    7320201259

--- public/js/script.js
+++ public/js/script.js
@@ -5,18 +5,6 @@ let typingnameSet = new Set();
 var typingName = "";
 
 
-var MongoClient = require('mongodb').MongoClient;
-//Create a database named "mydb":
-var url = "mongodb://localhost:3000/mydb";
-
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
-});
-
-
-
 //room banabo, user table maintain
 const usernameCatcher = () => {
     let loaderPrompt = prompt("Enter your name : ", "your name here");
