ad6bb95c131b220499c3d97addeba2564d61730b
subhjoydey <subhojoy16@gmail.com>
2020-07-06 13:13:01 -0400

    742020530

--- public/js/script.js
+++ public/js/script.js
@@ -31,10 +31,35 @@ const roomCatcher = () => {
     $('#roomJoin').modal({ dismissible: false }).modal('open');
     document.getElementById('room_name').focus();
 
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-        console.log(response.toArray());
-    });
+
+
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId: 'BinarY',
+            cookie: true,
+            xfbml: true,
+            version: 'v7.0'
+        });
+
+
+        FB.getLoginStatus(function(response) {
+            console.log(response);
+        });
+
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) { return; }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+
+
+
 
 
     $("#roomForm").submit((e) => {
